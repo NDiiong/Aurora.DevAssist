e81055c0ce4927b1d7a6a8d3c1ae4fba19d395eb
N.Duong <nb.duong@outlook.com>
2023-09-30 22:01:27 +0700

    :apple:

--- ClassGenerator/CodeRefactorings/RequestCodeRefactoringProvider.cs
+++ ClassGenerator/CodeRefactorings/RequestCodeRefactoringProvider.cs
@@ -133,7 +133,7 @@ namespace ClassGenerator.CodeRefactorings
                 }
             }
 
-            return solution;
+            return default;
         }
 
         private class DocumentContext
